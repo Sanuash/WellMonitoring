66300dda6eac86e5e0fc9c5e0bffdade953486fc
Sanuash <wagrytann@gmail.com>
2025-05-31 15:35:03 +0300

    Update catboost_model.py

--- backend/catboost_model.py
+++ backend/catboost_model.py
@@ -3,7 +3,8 @@
 import pandas as pd
 from catboost import CatBoostClassifier
 
-MODEL_PATH = "catboost_model.cbm"
+# Укажите путь к файлу с моделью
+MODEL_PATH = "catboost_model_3.cbm"
 
 # Загружаем модель CatBoost
 model = CatBoostClassifier()
@@ -17,6 +18,7 @@ def predict(features: dict) -> float:
     :return: вероятность пескопроявления (например, вероятность для класса 1).
     """
     df = pd.DataFrame([features])
+    df['№ скв'] = df['№ скв'].astype(str)
     # Метод predict_proba возвращает вероятности для каждого класса
     proba = model.predict_proba(df)[0][1]
     return proba
